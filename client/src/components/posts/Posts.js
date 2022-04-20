f669887a55d87a4c0d7cc10f6aaed163769b642e
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-04-20 12:34:26 +0530

    Add files via upload

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,1 +1,14 @@
-
+import React from 'react'
+import Post from '../post/Post';
+import "./Posts.css";
+export default function Posts({posts}) {
+  return (
+    <div className='posts'>
+      {
+        posts.map((p,i)=>(
+          <Post post={p} key={i}/>
+        ))
+      }
+    </div>
+  )
+}
