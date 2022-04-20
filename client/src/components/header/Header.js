78235f61e581c2d1a66c098f3a950c5617a453ef
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-04-20 12:05:08 +0530

    Add files via upload

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,1 +1,13 @@
-
+import React from 'react'
+import "./Header.css";
+export default function Header() {
+  return (
+    <div className='header'>
+        <div className='headerTitles'>
+            <span className='headerTitlesm'>React & Node</span>
+            <span className='headerTitlelg'>Blog</span>
+        </div>
+        <img className="headerImage" src='Images/bus.jpg' alt='bus'/>
+    </div>
+  )
+}
