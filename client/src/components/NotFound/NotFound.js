aa16755135e7a47390757b4e43a81276891f3524
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-04-20 12:31:38 +0530

    Add files via upload

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -1,1 +1,16 @@
-
+import React from "react";
+import "./NotFound.css";
+import { Link } from "react-router-dom";
+export default function NotFound() {
+  return (
+    <div className="notFound">
+      <div className="notFoundHeading">
+      <span className="notFoundTitle1">ERROR 404</span>
+      <span className="notFoundTitle2">Page You are looking for is Not Found...</span>
+     </div>
+      <button className="notFoundHomeButton">
+        <Link className="link" to="/">Back</Link>
+      </button>
+    </div>
+  );
+}
